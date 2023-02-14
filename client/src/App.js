930fffa9fac258450d2093eea4803bd53cb948a7
Innovanito <khg05311@naver.com>
2023-02-14 17:11:39 +0900

    230214 COSR error occured

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,79 @@
 import React, { useState } from 'react';
 import house_img from './assets/house_img.jpg';
 import { useEffect } from 'react';
+import axios from 'axios';
 
 
 const App = () => {
-  const [locations, setLocations] = useState([])
+  const [area, setArea] = useState()
+  const [bhk, setBhk] = useState()
+  const [bathrooms, setBathrooms] = useState()
+  const [location, setLocation] = useState([])
   const [predictedPrice, setPredictedPrice] = useState()
-
-  const onChangeSelect = (e) => {
-    console.log(e.target.value)
-  }
 
   const onClickedEstimatePrice = () => {
     console.log("Estimate Price Button Clicked")
   }
 
+  useEffect(() => {
+    console.log("Area: ", area)
+    console.log("BHK: ", bhk)
+    console.log("Bathrooms: ", bathrooms)
+    console.log("Location: ", location)
+
+  }, [area, bhk, bathrooms, location])
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:5000/get_location_names')
+      .then(res => {
+        console.log(res)
+        setLocation(res.data.locations)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [location])
+  
+  
+  
+
   return (
     <div className=' flex flex-col justify-center items-center' >
-      <h className='mt-10 text-4xl'>Let me predict your Real Easte you looking for!</h>
+      <h1 className='mt-10 text-4xl'>Let me predict your Real Easte you looking htmlFor!</h1>
       <form className='flex flex-col justify-center items-center'>
         <h2 className=' text-3xl p-5 mt-10'>Area (Square Feet)</h2>
-        <input type="number"  className=' text-3xl mb-3 rounded-lg p-2 ' />
+        <input type="number"  className=' text-3xl mb-3 rounded-lg p-2 ' onChange={e => setArea(e.target.value)} />
         <h2 className=' text-2xl mb-2'>Choose your BHK</h2>
         
-        <ul class="items-center text-sm font-medium text-gray-200 bg-white-border border border-gray-500 rounded-lg sm:flex bg-cyan-100">
-          <li class="w-14 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-            <div class="flex items-center pl-3">
-                <input id="horizontal-list-radio-license" type="radio" value="1" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-100 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-100 dark:focus:ring-offset-gray-200 focus:ring-2 dark:bg-gray-200 dark:border-gray-200"/>
-                <label for="horizontal-list-radio-license" class="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">1 </label>
+        <ul className="items-center text-sm font-medium text-gray-200 bg-white-border border border-gray-500 rounded-lg sm:flex bg-cyan-100">
+          <li className="w-14 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+            <div className="flex items-center pl-3">
+                <input id="horizontal-list-radio-license" onChange={e => setBhk(e.target.value)} type="radio" value="1" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-100 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-100 dark:focus:ring-offset-gray-200 focus:ring-2 dark:bg-gray-200 dark:border-gray-200"/>
+                <label htmlFor="horizontal-list-radio-license" className="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">1 </label>
               </div>
           </li>
-          <li class="w-14 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-              <div class="flex items-center pl-3">
-                  <input id="horizontal-list-radio-id" type="radio" value="2" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                  <label for="horizontal-list-radio-id" class="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">2</label>
+          <li className="w-14 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+              <div className="flex items-center pl-3">
+                  <input id="horizontal-list-radio-id" onChange={e => setBhk(e.target.value)} type="radio" value="2" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                  <label htmlFor="horizontal-list-radio-id" className="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">2</label>
               </div>
           </li>
-          <li class="w-14 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-              <div class="flex items-center pl-3">
-                  <input id="horizontal-list-radio-millitary" type="radio" value="3" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                  <label for="horizontal-list-radio-millitary" class="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">3</label>
+          <li className="w-14 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+              <div className="flex items-center pl-3">
+                  <input id="horizontal-list-radio-millitary" onChange={e => setBhk(e.target.value)} type="radio" value="3" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                  <label htmlFor="horizontal-list-radio-millitary" className="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">3</label>
               </div>
           </li>
-          <li class="w-14 dark:border-gray-600">
-              <div class="flex items-center pl-3">
-                  <input id="horizontal-list-radio-passport" type="radio" value="4" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                  <label for="horizontal-list-radio-passport" class="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">4</label>
+          <li className="w-14 dark:border-gray-600">
+              <div className="flex items-center pl-3">
+                  <input id="horizontal-list-radio-passport" onChange={e => setBhk(e.target.value)} type="radio" value="4" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                  <label htmlFor="horizontal-list-radio-passport" className="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">4</label>
               </div>
           </li>
-          <li class="w-16 dark:border-gray-600 sm:border-l">
-              <div class="flex items-center pl-3">
-                  <input id="horizontal-list-radio-passport" type="radio" value="5" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                  <label for="horizontal-list-radio-passport" class="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">5</label>
+          <li className="w-16 dark:border-gray-600 sm:border-l">
+              <div className="flex items-center pl-3">
+                  <input id="horizontal-list-radio-passport" onChange={e => setBhk(e.target.value)} type="radio" value="5" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                  <label htmlFor="horizontal-list-radio-passport" className="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">5</label>
               </div>
           </li>
         </ul>
@@ -60,43 +82,43 @@ const App = () => {
       <form className='flex flex-col justify-center items-center mt-5'>
         <h2 className=' text-2xl mb-2'>Choose your Bathrooms</h2>
         
-        <ul class="items-center w-full text-lg font-medium text-gray-200 bg-white- border border-gray-500 rounded-lg sm:flex bg-cyan-100">
-          <li class="w-14 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 ">
-            <div class="flex items-center pl-3">
-                <input id="horizontal-list-radio-license" type="radio" value="1" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-100 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-100 dark:focus:ring-offset-gray-200 focus:ring-2 dark:bg-gray-200 dark:border-gray-200"/>
-                <label for="horizontal-list-radio-license" class="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">1 </label>
+        <ul className="items-center w-full text-lg font-medium text-gray-200 bg-white- border border-gray-500 rounded-lg sm:flex bg-cyan-100">
+          <li className="w-14 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 ">
+            <div className="flex items-center pl-3">
+                <input id="horizontal-list-radio-license" onChange={e => setBathrooms(e.target.value)} type="radio" value="1" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-100 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-100 dark:focus:ring-offset-gray-200 focus:ring-2 dark:bg-gray-200 dark:border-gray-200"/>
+                <label htmlFor="horizontal-list-radio-license" className="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">1 </label>
               </div>
           </li>
-          <li class="w-14 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-              <div class="flex items-center pl-3">
-                  <input id="horizontal-list-radio-id" type="radio" value="2" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                  <label for="horizontal-list-radio-id" class="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">2</label>
+          <li className="w-14 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+              <div className="flex items-center pl-3">
+                  <input id="horizontal-list-radio-id" onChange={e => setBathrooms(e.target.value)} type="radio" value="2" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                  <label htmlFor="horizontal-list-radio-id" className="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">2</label>
               </div>
           </li>
-          <li class="w-14 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-              <div class="flex items-center pl-3">
-                  <input id="horizontal-list-radio-millitary" type="radio" value="3" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                  <label for="horizontal-list-radio-millitary" class="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">3</label>
+          <li className="w-14 border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+              <div className="flex items-center pl-3">
+                  <input id="horizontal-list-radio-millitary" onChange={e => setBathrooms(e.target.value)} type="radio" value="3" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                  <label htmlFor="horizontal-list-radio-millitary" className="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">3</label>
               </div>
           </li>
-          <li class="w-14 dark:border-gray-600">
-              <div class="flex items-center pl-3">
-                  <input id="horizontal-list-radio-passport" type="radio" value="4" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                  <label for="horizontal-list-radio-passport" class="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">4</label>
+          <li className="w-14 dark:border-gray-600">
+              <div className="flex items-center pl-3">
+                  <input id="horizontal-list-radio-passport" onChange={e => setBathrooms(e.target.value)} type="radio" value="4" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                  <label htmlFor="horizontal-list-radio-passport" className="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">4</label>
               </div>
           </li>
-          <li class=" w-14 dark:border-gray-600 sm:border-l">
-              <div class="flex items-center pl-3">
-                  <input id="horizontal-list-radio-passport" type="radio" value="5" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                  <label for="horizontal-list-radio-passport" class="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">5</label>
+          <li className=" w-14 dark:border-gray-600 sm:border-l">
+              <div className="flex items-center pl-3">
+                  <input id="horizontal-list-radio-passport" onChange={e => setBathrooms(e.target.value)} type="radio" value="5" name="list-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
+                  <label htmlFor="horizontal-list-radio-passport" className="w-full py-3 ml-2 text-lg font-medium text-gray-900 dark:text-gray-600">5</label>
               </div>
           </li>
         </ul>
         <h2 className=' mt-5 text-2xl'>Locaiton</h2>
-        <select id="countries" class="bg-gray-50 border border-gray-100 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-400 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <select id="countries" className="bg-gray-50 border border-gray-100 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-400 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
           <option >Choose a location</option>
         </select>
-        <button className=' bg-blue-300 w-60 rounded-lg pointer-events-auto my-8 text-2xl border border-green-200 p-2' onclick={onClickedEstimatePrice} type="button">Estimate the price!</button>
+        <button className=' bg-blue-300 w-60 rounded-lg pointer-events-auto my-8 text-2xl border border-green-200 p-2' onClick={onClickedEstimatePrice} type="button">Estimate the price!</button>
         <div className='flex justify-center '>
           <div className=' bg-green-400 w-60 rounded-lg text-center text-lg p-2'>{predictedPrice}  <span>Lakh</span></div>
         </div>
